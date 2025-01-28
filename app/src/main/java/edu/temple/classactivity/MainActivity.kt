e06f567234cb250c0c09ddb2b647b776e24f1beb
tup94846 <150381476+tup94846@users.noreply.github.com>
2025-01-28 12:16:48 -0500

    Basic View

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,24 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.button)!!).setOnClickListener{
+            clickMeButton.text = "Button clicked!"
+            }
+        }
     }
-}
